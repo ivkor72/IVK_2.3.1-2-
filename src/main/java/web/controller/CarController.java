deac8ba1bc2dd34b9e851f0adb49e2041bcc7bee
fork_IVK <ivkor72@mail.ru>
2024-09-06 13:54:57 +0300

    6

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,14 +1,12 @@
 package web.controller;
 
-import model.Cars;
+import model.Car;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import service.CarService;
 import service.CarServiceImpl;
-
-
 import java.util.List;
 
 @Controller
@@ -16,14 +14,11 @@ public class CarController {
 
     private CarService carService = new CarServiceImpl();
 
-
-
     @GetMapping(value = "/cars")
-    public String Cars(@RequestParam(defaultValue = "5", value = "carsCount") Long carsCount, Model model) {
-        if (carsCount > 5 | carsCount <=0) carsCount = Long.valueOf(5);
-        List<Cars> carsList = carService.getCars(carsCount);
+    public String showCars(@RequestParam(defaultValue = "5", value = "carsCount") Long carsCount, Model model) {
+        if (carsCount > 5 | carsCount <= 0) carsCount = 5L;
+        List<Car> carsList = carService.getCars(carsCount);
         model.addAttribute("carsList", carsList);
         return "cars";
     }
-
 }
